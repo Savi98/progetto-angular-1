77e658a12668c588b0f3cdeb66b279da68259b71
Savi98 <savino.lan98@gmail.com>
2023-05-23 16:26:49 +0200

    HashLocationStrategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-import { HashLocationStrategy, LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -48,7 +48,7 @@ import { HashLocationStrategy, LocationStrategy, PathLocationStrategy } from '@a
     RouterModule
   ],
   providers: [
-    {provide : LocationStrategy , useClass : PathLocationStrategy }
+    {provide : LocationStrategy , useClass : HashLocationStrategy }
   ],
   bootstrap: [AppComponent]
 })
